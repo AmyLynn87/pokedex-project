b6d10090507437e906b4da53b73d61ddbabbc23e
AmyLynn87 <e.crescentini87@gmail.com>
2021-07-23 10:34:22 +0200

    fixed a bug on Navbar that not works when collapsed

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -3,18 +3,20 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand ms-3" href="/">Pokémon</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
-      </li>
-     
-    </ul>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Pokémon</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+       
+      </ul>
+    </div>
   </div>
 </nav>
     </div>
